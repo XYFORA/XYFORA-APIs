f36d3eb9e50e6021e9df02d10e5fd6cdfb378b4c
Ahmed Saleem Shaikh <main.xyfora@gmail.com>
2025-12-02 16:12:05 +0500

    Final Commit

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,14 @@
 import { PrismaClient } from "../generated/prisma/client";
 import "dotenv/config";
 
-const prisma = new PrismaClient();
+const globalForPrisma = globalThis as unknown as {
+    prisma: PrismaClient | undefined;
+};
 
-export { prisma };
+export const prisma =
+    globalForPrisma.prisma ??
+    new PrismaClient({
+        log: ["query", "info", "warn", "error"],
+    });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
